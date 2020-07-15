e0a47ee30b8c4314643f196dbe38cfb3c5d794b3
Dong Ik Lee <dilee@dnizzle-2.local>
2020-07-15 08:10:45 -0400

    WIP of adding stripe

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static('views'));
 const port = process.env.PORT || 5000;
 
+const stripe = require('stripe')('sk_test_51H4TqMCUChgYYpJHoIoxwatxynwWVNVPSAXgcJh53KJfP7sI4pk13bVNb82eKxfXQZqtPow8CFFLUmcmnd8IGAYk005NPLX37Y');
+
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: 1099,
+  currency: 'usd',
+  // Verify your integration in this guide by including this parameter
+  metadata: {integration_check: 'accept_a_payment'},
+});
+
+app.get('/secret', async (req, res) => {
+  const intent = // ... Fetch or create the PaymentIntent
+  res.json({client_secret: intent.client_secret});
+});
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
